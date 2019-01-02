4d47bf52f4c8ab48c2347a6fcbc21f6122dbd9ee
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2019-01-02 19:30:45 +0330

    1. Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+
+var App = React.createClass({
+    render : function(){
+        return (
+            <p> Hello World </p>
+        )
+    }
+});
+
+
+ReactDom.render(<App /> , document.getElementById('app') )
